ba41824d8ebd26207d8a96a9dcb0982e64c67feb
rjanahi <reemk2003@gmail.com>
2024-10-19 21:39:13 +0300

    game

---
+++ pacman.js
@@ -0,0 +1,46 @@
+
+class Pacman{
+    constructor(x,y,width,height,speed){
+        this.x=x
+        this.y=y
+        this.width=width
+        this.height=height
+        this.speed=speed
+    }
+
+    moveProcess(){
+
+    }
+    eat(){
+
+    }
+
+    moveBackwards(){
+
+    }
+
+    moveForwards(){
+
+    }
+
+    checkCollision(){
+
+    }
+
+    checkGhostCollision(){
+
+
+    }
+
+    checkDirectionIfPossible(){
+
+    }
+
+    changeAnimation(){
+
+    }
+
+    draw(){
+        
+    }
+}
